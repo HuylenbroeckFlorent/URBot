c5c7e3c039392f7a59cc493e6882f6568f0eae3f
Huylenbroeck Florent <huylenbroeck.florent@gmail.com>
2020-12-10 15:13:41 +0100

    Update smart_seller.py

--- python/auto_seller/smart_seller.py
+++ python/auto_seller/smart_seller.py
@@ -3,8 +3,6 @@ import requests
 from lxml import html
 
 def sell_card(id_perso_joueur, price, action='sellToPublic', buyer_name=''):
-    global params
-    
     data = {}
     data['price'] = str(price)+'^'
     data['action'] = str(action)+'^'
@@ -13,5 +11,3 @@ def sell_card(id_perso_joueur, price, action='sellToPublic', buyer_name=''):
     data['id_perso_joueur']=str(id_perso_joueur)
     ret = requests.post('https://www.urban-rivals.com/ajax/collection/sell_card.php', data=data, cookies=cookies, headers=headers)
     print(ret.text)
-    
-
